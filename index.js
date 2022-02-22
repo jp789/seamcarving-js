6acde9772b72627f70ab05544d1bbf6cbd78d3c1
juliaan p <juliaanp123@gmail.com>
2022-02-22 17:29:11 -0500

    i am quite close to using python + flask instead of opencv js...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // https://stackoverflow.com/a/64744763 how to blur, take image gradient w/ soble, and normalize
 
 const { createCanvas, loadImage } = require('canvas');
-const { writeFileSync,} = require("fs");
+const { writeFile, stat, mkdir} = require("fs");
 const {loadOpenCV, installDOM} = require("./utils");
 
 (async () => {
@@ -19,10 +19,20 @@ const {loadOpenCV, installDOM} = require("./utils");
 
   cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
 
-  // TODO add Gaussian blur before sobeling, and normalize after if needed
+  // TODO don't think blur is needed, or normalization for that matter
 
   cv.Sobel(src, dstx, cv.CV_8U, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
   cv.Sobel(src, dsty, cv.CV_8U, 0, 1, 3, 1, 0, cv.BORDER_DEFAULT);
+
+  let sobelx2 = new cv.Mat();
+  let sobely2 = new cv.Mat();
+  cv.multiply(dstx, dstx, sobelx2);
+  cv.multiply(dsty, dsty, sobely2);
+
+  // TODO take magnitude or sqrt of sobelx2 + sobelxy 
+  // learn about matrix operations in opencv js here 
+  // https://docs.opencv.org/3.4/dd/d4d/tutorial_js_image_arithmetics.html
+
   
   const canvasOutputx = createCanvas(300, 300);
   const canvasOutputy = createCanvas(300, 300);
@@ -30,13 +40,20 @@ const {loadOpenCV, installDOM} = require("./utils");
   cv.imshow(canvasOutputx, dstx);
   cv.imshow(canvasOutputy, dsty);
 
-  // TODO write files to output folder
-  // e.g. https://stackoverflow.com/questions/16316330/
-  writeFileSync('outputX.jpg', canvasOutputx.toBuffer('image/jpeg'));
-  writeFileSync('outputY.jpg', canvasOutputy.toBuffer('image/jpeg'));
+  // move file utils below to separate file/function
+  // https://stackoverflow.com/a/26815894/16952248
+  // writeFile('outputX.jpg', canvasOutputx.toBuffer('image/jpeg'), (err) => {
+  //   console.log("done writing outputx");
+  // });
+  
+  // writeFile('outputY.jpg', canvasOutputy.toBuffer('image/jpeg'), (err) => {
+  //   console.log("done writing outputy");
+  // });
 
   src.delete(); 
   dstx.delete();
   dsty.delete();
+  sobelx2.delete();
+  sobely2.delete();
 })();
 
